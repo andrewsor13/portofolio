70d3ae57b7c029b73f8ce1e541974335957fbb41
Ciobanca Andrei Sorin <ciobanca.andreisorin@gmail.com>
2024-12-20 07:35:09 +0200

    reduced images size

--- src/db/db.js
+++ src/db/db.js
@@ -1,18 +1,15 @@
 import exam1 from "../assets/ExamPractice 1.png";
-import exam2 from "../assets/ExamPractice 2.png";
 
 import movie1 from "../assets/MoviesProject 1.png";
-import movie2 from "../assets/MoviesProject 2.png";
 
 import webStudio1 from "../assets/WebStudio 1.png";
-import webStudio2 from "../assets/WebStudio 2.png";
 
 const data = [
   {
     livePage: "https://andrewsor13.github.io/ExamPracticeApp",
     sourceCode: "https://github.com/andrewsor13/ExamPracticeApp",
     name: "Exam practice app",
-    images: [exam1, exam2],
+    images: [exam1],
     description:
       "An application meant for law graduates to prepare for the Bar exam or INM.",
   },
@@ -20,7 +17,7 @@ const data = [
     livePage: "https://andrewsor13.github.io/MoviesProject/",
     sourceCode: "https://github.com/andrewsor13/MoviesProject",
     name: "MoviesProject",
-    images: [movie1, movie2],
+    images: [movie1],
     description:
       "A movie search application with trending movies and details about them.",
   },
@@ -28,7 +25,7 @@ const data = [
     livePage: "https://andrewsor13.github.io/goit-markup-hw-08/index.html",
     sourceCode: "https://github.com/andrewsor13/goit-markup-hw-08",
     name: "WebStudio",
-    images: [webStudio1, webStudio2],
+    images: [webStudio1],
     description:
       "WebStudio website with a responsive layout, modal window and attractive design.",
   },
